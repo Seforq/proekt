7842a46da61150728aa2836c60812c7dbd8c757f
Sergo <youninja910@gmail.com>
2024-01-02 02:39:40 +0700

    очень большая наработка

---
+++ main/js/main.js
@@ -0,0 +1,12 @@
+VANTA.FOG({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 2000,
+    minWidth: 2000,
+    highlightColor: 0x7f7465,
+    midtoneColor: 0x1e1b19,
+    lowlightColor: 0x706b87,
+    baseColor: 0x0
+})
